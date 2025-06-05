1829095c5ac65aa7e8d8f3c1ef71db36aae6066b
Raji Moshood <rajimoshood48@gmail.com>
2025-06-05 21:31:24 +0100

    Python project

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 x = 1 + 1
 print (x)
+ice_cream = ['Chocolate','Vanilla','Rocky Road']
+x,y,z= ice_cream
+
+_testvariable_testvariable_testvariable_testvariable
 
 
-
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+avg=(a+b)/2
+print("Average of given two number : ", avg)
